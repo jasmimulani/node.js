6ec38c4299af931143a4e4b2150315019137e78e
Jasmin <jasmimulani@gmail.com>
2024-07-01 17:50:36 +0530

    "01/07/24"

--- MY-SQL/lecture-3/pri_forkey.sql
+++ MY-SQL/lecture-3/pri_forkey.sql
@@ -12,18 +12,20 @@ use foreignkey;
     baldue int (10)
   );
 
---  insert into client_master1 values
--- ('C01' , 'jasmi' ,'33/A' , 'jakatnaka' , ' surat' ,395006 , 'gujarat',2000),
+  insert into client_master1 values
+ ('C01' , 'jasmi' ,'33/A' , 'jakatnaka' , ' surat' ,395006 , 'gujarat',2000);
+ 
+ insert into client_master1
+  values('C02' , 'srushti','44/A' , 'punagaam' , ' surat' ,395005 , 'gujarat',3000),
+ ('C03' , 'ashiti' ,'55/C' , 'amroli' , 'mumbai' ,395007 , 'gujarat',7000),
+   ('C04' , 'vishva' ,'66/B' , 'amroli' , ' pune' ,395007 , 'gujarat',0),
+   ('C05' , 'krisha' ,'25/D' , 'nanavaracha' , ' maharashtra' ,395004 , 'gujarat',0),
+     ('C06' , 'vinal' ,'76/B' , 'amroli' , ' bangalore' ,560001 , 'karnataka',0);
+      select*from client_master1;
 
---  insert into client_master1
---  values('C02' , 'srushti','44/A' , 'punagaam' , ' surat' ,395005 , 'gujarat',3000),
--- ('C03' , 'ashiti' ,'55/C' , 'amroli' , 'mumbai' ,395007 , 'gujarat',7000),
---   ('C04' , 'vishva' ,'66/B' , 'amroli' , ' pune' ,395007 , 'gujarat',0),
---   ('C05' , 'krisha' ,'25/D' , 'nanavaracha' , ' maharashtra' ,395004 , 'gujarat',0),
---     ('C06' , 'vinal' ,'76/B' , 'amroli' , ' bangalore' ,560001 , 'karnataka',0);
 
    
--- table 2--------------------------------------------------------------------------------------------------------
+-- table 2
 
  create table PRODUCT_MASTER1 (
    PRODUCTNO varchar (6) primary key ,
@@ -36,18 +38,20 @@ use foreignkey;
    COSTPRICE int (8) not null 
  );
 
- -- insert into PRODUCT_MASTER1
-   -- values('P001' , 'T-shirts' , 5 ,'piece' , 200 , 50 ,350 , 250),
---         ('P002','shirts',7,'piece' , 150,50,500,350), 
---         ('P003','denim shirts',6,'piece' , 150,20,100,250), 
---         ('P004','lycra tpo',5,'piece' , 150,20,700,450), 
---        ('P005','Trousers',2,'piece' , 350,40,450,350), 
---         ('P006','cotton jeans',2,'piece' , 150,30,350,220), 
---         ('P007','skirts',4,'piece' , 250,60,200,800),
---         ('P008','jeans',3,'piece' , 650,60,600,700),
---         ('P009','pull overs',1,'piece' , 450,60,900,500);
+  insert into PRODUCT_MASTER1
+    values('P001' , 'T-shirts' , 5 ,'piece' , 200 , 50 ,350 , 250),
+        ('P002','shirts',7,'piece' , 150,50,500,350), 
+         ('P003','denim shirts',6,'piece' , 150,20,100,250), 
+         ('P004','lycra tpo',5,'piece' , 150,20,700,450), 
+        ('P005','Trousers',2,'piece' , 350,40,450,350), 
+         ('P006','cotton jeans',2,'piece' , 150,30,350,220), 
+        ('P007','skirts',4,'piece' , 250,60,200,800),
+         ('P008','jeans',3,'piece' , 650,60,600,700),
+         ('P009','pull overs',1,'piece' , 450,60,900,500);
+         select * from product_master1;
 
  -- desc product_master1
+ 
 
 -- table 3 ----------------------------------------------------------------------------------
 
@@ -65,11 +69,13 @@ use foreignkey;
      REMARKS varchar(60)
   );
 
---  insert into SALESMAN_MASTER1
---  values('S0001' , 'janvi' ,'14/E' , 'worli' , 'mumbai' ,400002 , 'maharashtra',2000 , 100 , 50 , 'Good'),
---  ('S0002' , 'rajvi' ,'65' , 'jakatnaka' , ' surat' ,395006 , 'gujarat',2000,200 , 100 , 'Good'),
--- ('S0003' , 'khanak' ,'33/A' , 'juhu' , ' mumbai' ,400004, 'maharashtra',2000 , 200 , 150, 'Good'),
--- ('S0004' , 'palak' ,'3/A' , 'puna' , 'surat' ,395004, 'gujarat',2000 , 100 , 150, 'Good');
+  insert into SALESMAN_MASTER1
+  values('S0001' , 'janvi' ,'14/E' , 'worli' , 'mumbai' ,400002 , 'maharashtra',2000 , 100 , 50 , 'Good'),
+  ('S0002' , 'rajvi' ,'65' , 'jakatnaka' , ' surat' ,395006 , 'gujarat',2000,200 , 100 , 'Good'),
+ ('S0003' , 'khanak' ,'33/A' , 'juhu' , ' mumbai' ,400004, 'maharashtra',2000 , 200 , 150, 'Good'),
+ ('S0004' , 'palak' ,'3/A' , 'puna' , 'surat' ,395004, 'gujarat',2000 , 100 , 150, 'Good');
+ 
+  select * from salesman_master1;
 
 -- table 4--------------------------------------------------------------------------------
 
@@ -94,7 +100,7 @@ values ('o00001','c01','2022-08-12' ,'s0001','F','N','20-04-02','inprocess'),
 ('o00005','c03','2016-06-22' ,'s0004','F','Y','2019-05-02','cancelled'),
 ('o00006','c03','2021-01-13' ,'s0001','P','Y','2023-04-01','inprocess');
 
--- select * from sales_order
+select * from sales_order;
 
 --  table 5
 
@@ -125,3 +131,5 @@ values ('o00001','c01','2022-08-12' ,'s0001','F','N','20-04-02','inprocess'),
   ('o00006','p007' , 1 ,0 , 1050),
   ('o00002','p008' , 10 ,5 , 525),
   ('o00004','p001' , 5,3, 1050);
+  
+   select * from order_details
