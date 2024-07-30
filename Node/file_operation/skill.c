dd371da9d63097134a67dad99794920749e4707d
Jasmin <jasmimulani@gmail.com>
2024-07-30 20:59:46 +0530

    30/07/24

---
+++ Node/file_operation/skill.c
@@ -0,0 +1,133 @@
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+  
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+  
+  #include<stdio.h>
+  int main(){
+  printf("this is  data stote variable in print method");
+  return 0;
+  }
+ 
